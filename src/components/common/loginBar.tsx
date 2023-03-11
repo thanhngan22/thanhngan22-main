471dbbd637ea201f998806b5be95df3ad5ef334f
thanhngan22 <nganbui9a2017@gmail.com>
2023-03-11 14:25:27 +0700

    :hatched-chick: css for navbar and loginbar/ add fontawesome

--- src/components/common/loginBar.tsx
+++ src/components/common/loginBar.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faRightToBracket} from "@fortawesome/free-solid-svg-icons";
+import {faUserPlus} from "@fortawesome/free-solid-svg-icons";
 
+const faPIRightToBracket = faRightToBracket as IconProp;
+const faPIUserPlus = faUserPlus as IconProp;
 export default class LoginBar extends React.Component {
   render() {
     return (
-      <div className="LoginBar">
-        <h1>LoginBar</h1>
+      <div className="LoginBar w-2/12 flex items-center justify-end  ">
+        <div className=" px-4 py-1 border border-solid border-black rounded-full mr-2">
+        <a className=" w-14 text-sm " href="/login">
+            <FontAwesomeIcon className="text-gray-700 pl-2 pr-4" icon={faPIRightToBracket} fade  
+            />Login</a>
+        </div>
+     
+        <div className="px-5 py-1 border border-solid border-white rounded-full text-white bg-black">
+        <a className="text-sm" href="/signup">
+            <FontAwesomeIcon className="pr-1" icon={faPIUserPlus} 
+            /> Sign Up</a>
+        </div>
+          
       </div>
     );
   }
