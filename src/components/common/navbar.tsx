93f55d3bb9615d80bb11cef3154198217cc5fefa
thanhngan22 <nganbui9a2017@gmail.com>
2023-03-13 14:40:32 +0700

    :pear: edit css and background of header

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -4,44 +4,50 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faBookOpen, faBlog, faPen, faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
 
+interface Props {
+    name: string;
+    path: string;
+}
+
 export default class Navbar extends React.Component {
-    constructor(props: any) {
+    constructor(props: Props ) {
         super(props);
     }
 
-    // aria - current = "page"
+    
+
 
     render () {
     return (
-        <div className = "Home-navbar  h-12 w-4/12 rounded-full border-white border">
-            <ul className = "justify-around flex w-full h-full">
-                <li className=" py-1 self-center    ">
-                    <Link className="nav-item bg-blue-500 border rounded-full hover:border-red-400 text-white py-1 pl-3 pr-3 ml-2   hover:text-black hover:bg-gray-100 active:text-blue-400 py-1"  to = "/"
+        <div className = "Home-navbar h-9 w-4/12 ">
+            <ul className = "justify-around flex w-full ">
+                <li className="py-1 self-center   ">
+                    <Link aria-current="page"  className="px-1.5 py-px text-gray-300 font-thin hover:bg-gray-700 hover:opacity-50 border border-black hover:border-gray-600   hover:rounded-sm"  to = "/"
                         ><FontAwesomeIcon className="nav-fav pr-2" icon={faHome} 
                             /> Home
                     </Link> 
                 </li>
-                <li className=" py-1 self-center    ">
-                    <Link className="nav-item bg-blue-500 border rounded-full hover:border-red-400 text-white py-1 px-3.5  hover:text-black hover:bg-gray-100 active:text-blue-400 py-1"  to = "/docs"
+                <li className="py-1 self-center    ">
+                    <Link className="px-1.5 py-px text-gray-300 font-thin hover:bg-gray-700 hover:opacity-50 border border-black hover:border-gray-600   hover:rounded-sm"  to = "/docs"
                         ><FontAwesomeIcon className="nav-fav pr-2 " icon={faBookOpen} 
                             /> Docs
                     </Link> 
                     </li>
-                <li className=" py-1 self-center    ">
-                    <Link className="nav-item bg-blue-500 border rounded-full hover:border-red-400 text-white py-1 px-3.5  hover:text-black hover:bg-gray-100 active:text-blue-400 py-1"  to = "/blog"
+                <li className="py-1 self-center    ">
+                    <Link className="px-1.5 py-px text-gray-300 font-thin hover:bg-gray-700 hover:opacity-50 border border-black hover:border-gray-600   hover:rounded-sm"  to = "/blog"
                         ><FontAwesomeIcon className="nav-fav pr-3 " icon={faBlog} 
                             /> Blog
                     </Link> 
                     </li>
-                <li className=" py-1 self-center    ">
-                    <Link className="nav-item bg-blue-500 border rounded-full hover:border-red-400 text-white py-1 px-2.5  hover:text-black hover:bg-gray-100 active:text-blue-400 py-1"  to = "/writing"
+                <li className="py-1 self-center    ">
+                    <Link className="px-1.5 py-px text-gray-300 font-thin hover:bg-gray-700 hover:opacity-50 border border-black hover:border-gray-600   hover:rounded-sm"  to = "/writing"
                         ><FontAwesomeIcon className="nav-fav pr-1" icon={faPen} 
                             /> Writing
                     </Link>
                     </li>
 
-                <li className=" py-1 self-center    ">
-                    <Link className="nav-item bg-blue-500 border rounded-full hover:border-red-400 text-white py-1 px-3.5 mr-2  hover:text-black hover:bg-gray-100 active:text-blue-400 py-1"  to = "/about"
+                <li className="py-1 self-center    ">
+                    <Link className="px-1.5 py-px text-gray-300 font-thin hover:bg-gray-700 hover:opacity-50 border border-black hover:border-gray-600   hover:rounded-sm"  to = "/about"
                         ><FontAwesomeIcon className="nav-fav pr-2" icon={faUserAstronaut} 
                             /> Study
                     </Link> 
