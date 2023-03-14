03558b4922ffa8e7e0a828fbe566eff941232f48
thanhngan22 <nganbui9a2017@gmail.com>
2023-03-14 10:34:45 +0700

    config: install axios and dotenv/ connect with be

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './layout/Header';
-import Body from './layout/Body';
 import Footer from './layout/Footer';
 
 import Home from './pages/Home';
@@ -22,7 +21,6 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Body />
         <Routes>
           <Route path='/home' element={<Home />} />
           <Route path="/blog" element={<Blog />} />
