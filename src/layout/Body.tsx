bbe19903239d336ae9aa8391a5dd569584ec0a47
thanhngan22 <nganbui9a2017@gmail.com>
2023-03-16 09:08:38 +0700

    feat: edit routes/ add outlet for rendering content in the middle

--- src/layout/Body.tsx
+++ src/layout/Body.tsx
@@ -6,20 +6,18 @@ interface BodyProps {
 
 export default function Body (content: BodyProps) {
     return (
-        <div className = "body">
-            <div className = "body__container mt-10 flex w-full">
-                <div className="tab__left pl-8 w-3/12 min-h-screen text-white bg-black">
+        <div className = "body flex">
+                <div className="pt-10 tab__left pl-8 w-3/12 min-h-screen text-white bg-black">
                     {/* <h1 className="tab__left__title ">Tab left</h1> */}
                     <content.TabLeft />
                 </div>
-                <div className="body__content w-6/12 text-white bg-black  border-r border-gray-600 border-l border-opacity-50 ">
+                <div className="pt-10 body__content w-6/12 text-white bg-black  border-r border-gray-600 border-l border-opacity-50 ">
                     {/* <h1 className="body__content__title ">Body content</h1> */}
                     <content.BodyContent />
                 </div>
-                <div className="tab__right w-3/12 text-white bg-black   pr-3">
+                <div className="pt-10 tab__right w-3/12 text-white bg-black   pr-3">
                     {/* <h1 className="tab__right__title">Tab right</h1> */}
                     <content.TabRight />
-                </div>
             </div>
         </div>
     )
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,13 @@
 import Header from "./Header";
 import Footer from "./Footer";
 
+import { Outlet } from "react-router-dom";
+
 export default function Layout () {
     return (
-        <div className="container" >
+        <div className="container max-w-full " >
             <Header/>
+            <Outlet/>
             <Footer/>
         </div>
     )
