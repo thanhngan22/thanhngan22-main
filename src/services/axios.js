63a195163f3ebeb87201f19a518cd121fa1248d2
thanhngan22 <nganbui9a2017@gmail.com>
2023-05-09 23:38:47 +0700

    add dotenv and config environtment variables

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
+const apiURL = process.env.REACT_APP_API_BASE_URL;
+console.log("api backend url" , apiURL)
 
 const instance = axios.create({
-  baseURL: 'localhost:3001',
+  baseURL: apiURL,
   timeout: 10000, // Thời gian chờ tối đa khi gửi yêu cầu
 
 });
+
+
 
 export default instance;
